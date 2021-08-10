96968ae6559fee068fae048f5925d4e3a628ed78
moxicat <59305689+moxicode@users.noreply.github.com>
2021-08-10 14:23:11 +0800

    Delete Library\JS\Api.js

--- Library\JS\Api.js
+++
@@ -1,9 +0,0 @@
-
-/// <reference path="./Basic/Block.js" />
-/// <reference path="./Basic/Entity.js" />
-/// <reference path="./Basic/FloatPos.js" />
-/// <reference path="./Basic/IntPos.js" />
-/// <reference path="./Basic/Item.js" />
-/// <reference path="./Basic/mc.js" />
-/// <reference path="./Basic/Player.js" />
-/// <reference path="./Object/ObjectRuncmdEx.js" />
