2e4fe49ad0b3768929a1b4640111f59f4a08d6c5
moxicat <3601594248@qq.com>
2021-08-12 13:15:15 +0000

    update

--- Library/Lua/Basic/mc.lua
+++ Library/Lua/Basic/mc.lua
@@ -94,7 +94,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onPreJoin" 玩家开始连接服务器
+---@param event ""onPreJoin"" 玩家开始连接服务器
 ---@param callback fun(player:Player): 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -109,7 +109,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onJoin" 玩家进入游戏（加载世界完成）
+---@param event ""onJoin"" 玩家进入游戏（加载世界完成）
 ---@param callback fun(player:Player): 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -124,7 +124,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onLeft" 玩家离开游戏
+---@param event ""onLeft"" 玩家离开游戏
 ---@param callback fun(player:Player): 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -139,7 +139,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onRespawn" 玩家重生
+---@param event ""onRespawn"" 玩家重生
 ---@param callback fun(player:Player): 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -154,7 +154,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onPlayerDie" 玩家死亡
+---@param event ""onPlayerDie"" 玩家死亡
 ---@param callback fun(player:Player): 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -169,7 +169,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onPlayerCmd" 玩家执行命令
+---@param event ""onPlayerCmd"" 玩家执行命令
 ---@param callback fun(player:Player,cmd:string):boolean 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -184,7 +184,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onChat" 玩家发送聊天信息
+---@param event ""onChat"" 玩家发送聊天信息
 ---@param callback fun(player:Player,msg:string):boolean 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -199,7 +199,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onChangeDim" 玩家切换维度
+---@param event ""onChangeDim"" 玩家切换维度
 ---@param callback fun(player:Player): 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -214,7 +214,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onJump" 玩家跳跃
+---@param event ""onJump"" 玩家跳跃
 ---@param callback fun(player:Player): 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -229,7 +229,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onSneak" 玩家切换潜行状态
+---@param event ""onSneak"" 玩家切换潜行状态
 ---@param callback fun(player:Player,isSneaking:boolean): 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -244,7 +244,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onAttack" 玩家攻击实体
+---@param event ""onAttack"" 玩家攻击实体
 ---@param callback fun(player:Player,entity:Entity):boolean 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -259,7 +259,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onUseItem" 玩家使用物品
+---@param event ""onUseItem"" 玩家使用物品
 ---@param callback fun(player:Player,item:Item):boolean 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -274,7 +274,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onUseItemOn" 玩家对方块使用物品（点击右键）
+---@param event ""onUseItemOn"" 玩家对方块使用物品（点击右键）
 ---@param callback fun(player:Player,item:Item,block:Block):boolean 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -289,7 +289,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onTakeItem" 玩家捡起物品
+---@param event ""onTakeItem"" 玩家捡起物品
 ---@param callback fun(player:Player,entity:Entity):boolean 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -304,7 +304,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onDropItem" 玩家丢出物品
+---@param event ""onDropItem"" 玩家丢出物品
 ---@param callback fun(player:Player,item:Item):boolean 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -319,7 +319,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onEat" 玩家食用食物
+---@param event ""onEat"" 玩家食用食物
 ---@param callback fun(player:Player,item:Item): 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -334,7 +334,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onStartDestroyBlock" 玩家开始破坏方块 / 点击左键
+---@param event ""onStartDestroyBlock"" 玩家开始破坏方块 / 点击左键
 ---@param callback fun(player:Player,block:Block): 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -349,7 +349,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onDestroyBlock" 玩家破坏方块完成
+---@param event ""onDestroyBlock"" 玩家破坏方块完成
 ---@param callback fun(player:Player,block:Block):boolean 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -364,7 +364,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onPlaceBlock" 玩家放置方块
+---@param event ""onPlaceBlock"" 玩家放置方块
 ---@param callback fun(player:Player,block:Block):boolean 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -379,7 +379,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onOpenContainer" 玩家打开容器方块
+---@param event ""onOpenContainer"" 玩家打开容器方块
 ---@param callback fun(player:Player,block:Block):boolean 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -394,7 +394,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onCloseContainer" 玩家关闭容器方块
+---@param event ""onCloseContainer"" 玩家关闭容器方块
 ---@param callback fun(player:Player,block:Block):boolean 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -409,7 +409,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onInventoryChange" 玩家物品栏变化
+---@param event ""onInventoryChange"" 玩家物品栏变化
 ---@param callback fun(player:Player,slotNum:number,oldItem:Item,newItem:Item): 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -424,7 +424,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onMove" 玩家移动
+---@param event ""onMove"" 玩家移动
 ---@param callback fun(player:Player,pos:FloatPos): 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -439,7 +439,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onSetArmor" 玩家改变盔甲栏
+---@param event ""onSetArmor"" 玩家改变盔甲栏
 ---@param callback fun(player:Player,slotNum:number,item:Item): 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -454,7 +454,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onUseRespawnAnchor" 玩家使用重生锚
+---@param event ""onUseRespawnAnchor"" 玩家使用重生锚
 ---@param callback fun(player:Player,pos:IntPos):boolean 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
@@ -469,7 +469,7 @@ end
 ---避开误区
 ---有些时候，在某些事件监听内部调用特定的API会造成死循环崩服，请务必避免这些情况的发生
 ---举例：在onConsoleCmd事件监听中调用mc.runcmd(Ex)系列函数执行后台指令，将导致死循环
----@param event "onOpenContainerScreen" 玩家打开容器类GUI
+---@param event ""onOpenContainerScreen"" 玩家打开容器类GUI
 ---@param callback fun(player:Player):boolean 事件回调
 ---@return boolean 是否添加成功
 function mc.listen(event,callback)
