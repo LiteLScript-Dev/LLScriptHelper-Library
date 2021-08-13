dc12d6cacdbef106fd3a8cb6ca92555d45213f4e
moxicat <3601594248@qq.com>
2021-08-13 11:11:46 +0000

    update

--- Library/JS/Global/Global.js
+++ Library/JS/Global/Global.js
@@ -5,18 +5,21 @@
 
 /**
  * 输出信息到控制台\n这是为了方便输出信息而设计的便捷函数
- * @param {...any} data 
+ * @param {any} data1? 
+ * @param {any} data2? 
+ * @param {} data3? 
  * @returns {null} 
  */
- static log(...)
+ static log(data1,data2,data3)
 
 /**
  * 输出带颜色文本
  * @param {string} color 此行输出的颜色
- * @param {...any} data 
+ * @param {any} data1? 
+ * @param {any} data2? 
  * @returns {null} 
  */
- static colorLog(color,...)
+ static colorLog(color,data1,data2)
 
 /**
  * 推迟一段时间执行函数
--- Library/Lua/Global/Global.lua
+++ Library/Lua/Global/Global.lua
@@ -1,16 +1,19 @@
 
 
 ---输出信息到控制台\n这是为了方便输出信息而设计的便捷函数
----@vararg any
+---@param data1? any 
+---@param data2? any 
+---@param data3?  
 ---@return nil 
-function log(...)
+function log(data1,data2,data3)
 end
 
 ---输出带颜色文本
 ---@param color string 此行输出的颜色
----@vararg any
+---@param data1? any 
+---@param data2? any 
 ---@return nil 
-function colorLog(color,...)
+function colorLog(color,data1,data2)
 end
 
 ---推迟一段时间执行函数
